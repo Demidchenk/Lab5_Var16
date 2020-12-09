6fdb81d4c37ce964650bebad63b1271e2c8478a2
Алексей <alexey.demidchenko@lll.kpi.ua>
2020-12-09 09:34:21 +0200

    Add project files.

---
+++ Lab5_Var16/Lab5_Var16.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1, b = 1;
+    bool c = true;
+    while (b < 100)
+    {
+        b += a;
+        a = b - a;
+        c = true;
+        for (int i = 2; i < b / 2; i++)
+        {
+            if (b % i == 0) c = false;
+        }
+        if (c) cout << b << endl;
+    }
+    return 0;
+}
